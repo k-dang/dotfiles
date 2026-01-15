de14da40817d383cd574dd05662cdb887d00a964
Kevin Dang <dangkevin12@gmail.com>
2026-01-15 13:24:36 -0500

    fix: ga & gd aliases

--- modules/aliases.sh
+++ modules/aliases.sh
@@ -3,49 +3,37 @@ alias gst='git status'
 
 # create a new worktree + branch, then step into it
 ga() {
-	local branch="$1"
-	if [[ -z "$branch" ]]; then
-		echo "Usage: ga <branch-name>"
+	if [[ -z "$1" ]]; then
+		echo "Usage: ga [branch name]"
 		return 1
 	fi
 
-	local base=$(basename "$PWD")
-	local path="$(dirname "$PWD")/${base}--${branch}"
+	local branch="$1"
+	local base="$(basename "$PWD")"
+	local target="../${base}--${branch}"
 
-	git worktree add -b "$branch" "$path"
-	cd "$path"
+	git worktree add -b "$branch" "$target"
+	mise trust "$target"
+	cd "$target"
 }
 
 # remove current worktree and its branch
 gd() {
-	if ! gum confirm "Remove worktree and branch?"; then
-		echo "Action cancelled."
-		return
-	fi
+	if gum confirm "Remove worktree and branch?"; then
+		local cwd base branch root
 
-	local current_path="$PWD"
-	local worktree=$(basename "$current_path")
+		cwd="$(pwd)"
+		worktree="$(basename "$cwd")"
 
-	if [[ "$worktree" != *"--"* ]]; then
-		echo "Abort: Folder '$worktree' doesn't follow 'root--branch' convention." >&2
-		return 1
-	fi
+		# split on first `--`
+		root="${worktree%%--*}"
+		branch="${worktree#*--}"
 
-	local root="${worktree%%--*}"
-	local branch="${worktree#*--}"
-
-	local parent_path=$(dirname "$current_path")
-	local root_path="$parent_path/$root"
-
-	if [[ -d "$root_path" ]]; then
-		echo "Moving to $root_path..."
-		cd "$root_path"
-		echo "Removing worktree $worktree..."
+		# Protect against accidentially nuking a non-worktree directory
+		if [[ "$root" != "$worktree" ]]; then
+		cd "../$root"
 		git worktree remove "$worktree" --force
-		echo "Deleting branch $branch..."
 		git branch -D "$branch"
-	else
-		echo "Could not find root repository at $root_path" >&2
-		return 1
+		fi
 	fi
 }
